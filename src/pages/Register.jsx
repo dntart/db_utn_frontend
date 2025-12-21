e5a8cfe866c5c4683566a3905ec3ddc6190b6f73
dntart <dantecesarsegovia@gmail.com>
2025-12-21 09:27:19 -0800

    agregue navigate a login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 
 const Register = () => {
-    return <Layout>   {/*//Layout contiene header-main-footer*/}
-       <div className="center-container">
-      <form className="form-card centered-form">
+  const navigate = useNavigate()      //registrado va al login
+  //crear Estados de manejo de inputs
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+  //capturar inputs 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value   //1ro. seleccionamos dinamicamente el input por su nombre/2do.caputramos su valor
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {            /* enviamos al back la peticion */
+      const response = await fetch("http://localhost:3000/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      /* respuestas al usuario despues de validacion */
+      const responseData = await response.json()  //pasamos a js el json
+      if (!responseData.succes) {
+        return alert(responseData.error) //??
+      }
+      alert("✅usuario creado con exito")
+      
+      navigate("/login")
+    
+
+    } catch (error) {
+      console.log("error al registrarse el usuario")
+    }
+  }
+
+  return <Layout>   {/*//Layout contiene header-main-footer*/}
+    <div className="center-container">
+      <form onSubmit={handleSubmit} className="form-card centered-form" >
         <h2>Registrarse</h2>
 
         <label>Email</label>
-        <input type="email" placeholder="Correo electrónico" />
+        <input
+          name="email"
+          type="email"
+          placeholder="Correo electrónico"
+          onChange={handleChange}
+        />
 
         <label>Contraseña</label>
-        <input type="password" placeholder="Cree una contraseña" />
+        <input
+          name="password"
+          type="password"
+          placeholder="Cree una contraseña"
+          onChange={handleChange}
+        />
 
         <button type="submit">Crear Cuenta</button>
       </form>
     </div>
-    </Layout>
+  </Layout>
 };
 
 export default Register;
